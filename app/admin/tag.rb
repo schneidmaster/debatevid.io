3de879c2325a41d244a7644da2effc1b05cbb252
Zach Schneider <schneidmaster@gmail.com>
2015-04-04 12:45:49 -0500

    Allow all params in active admin

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -1,18 +1,9 @@
 ActiveAdmin.register Tag do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  controller do
+    def permitted_params
+      params.permit! # allow all parameters
+    end
+  end
 
 end
