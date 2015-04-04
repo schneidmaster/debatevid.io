1a1f6ea25317857ff14c4437bb68f7ba8890f682
Zach Schneider <schneidmaster@gmail.com>
2015-04-04 18:08:12 -0500

    Include all debaters

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -70,7 +70,7 @@ class VideosController < ApplicationController
     videos = videos.where(tournament: search_param(:tournament)) if search_param(:tournament)
     videos = videos.joins(aff_team: :school).joins(neg_team: :school).where('schools.id = ? or schools_teams.id = ?', search_param(:school), search_param(:school)) if search_param(:school)
     videos = videos.where('aff_team_id = ? or neg_team_id = ?', search_param(:team), search_param(:team)) if search_param(:team)
-    videos = videos.joins(aff_team: [:debater_one, :debater_two]).joins(neg_team:  [:debater_one, :debater_two]).where('debaters.id = ? or debater_twos_teams.id = ?', search_param(:debater), search_param(:debater)) if search_param(:debater)
+    videos = videos.joins(aff_team: [:debater_one, :debater_two]).joins(neg_team:  [:debater_one, :debater_two]).where('debaters.id = ? or debater_ones_teams.id = ? or debater_twos_teams.id = ? or debater_twos_teams_2.id = ?', search_param(:debater), search_param(:debater), search_param(:debater), search_param(:debater)) if search_param(:debater)
     videos = videos.joins(:tags).where('tags.title = ?', search_param(:tag)) if search_param(:tag)
     
     render partial: "videos/table", locals: { videos: videos }
