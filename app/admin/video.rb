fcf0ba48d7686bcf855d42d1509ba88e964803a5
Zach Schneider <schneidmaster@gmail.com>
2015-04-04 17:10:11 -0500

    AA form fixes

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,5 +1,26 @@
 ActiveAdmin.register Video do
+  form do |f|
+    input :user
+    input :tournament
+    input :aff_team
+    input :neg_team
+    input :debate_type, as: :select, collection: Video.debate_types.keys.to_a
+    input :debate_level, as: :select, collection: Video.debate_levels.keys.to_a
+    input :thumbnail
+    actions
+  end
+
   controller do
+    def create
+      params[:video][:key] = params[:video][:key].split(',').reject!(&:empty?)
+      super
+    end
+
+    def update
+      params[:video][:key] = params[:video][:key].split(',').reject!(&:empty?)
+      super
+    end
+
     def permitted_params
       params.permit! # allow all parameters
     end
