0408e9fc67961182bf7daf1efe1634631f46dccc
Zach Schneider <schneidmaster@gmail.com>
2015-04-05 16:32:29 -0500

    HTTPS avatars plz

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   class << self
     def find_or_create_from_auth_hash(auth_hash)
       user = User.find_or_create_by provider: auth_hash[:provider], uid: auth_hash[:uid]
-      user.avatar = auth_hash[:info][:image]
+      user.avatar = auth_hash[:info][:image].sub('http://', 'https://')
 
       user.name =
         if auth_hash[:info][:first_name]
