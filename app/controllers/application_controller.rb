ecc6b76e59e38a2ec01c8392d23a733a6f64de03
Zach Schneider <schneidmaster@gmail.com>
2015-07-13 10:50:17 -0500

    Redirect from Heroku URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :redirect_if_heroku
+
   def logged_in?
     session[:current_user].present?
   end
@@ -14,5 +16,11 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     redirect_to root_path unless logged_in? && current_user.is_admin
+  end
+
+  private
+
+  def redirect_if_heroku
+    redirect_to "https://debatevid.io#{request.fullpath}" if request.host == 'debatevidio.herokuapp.com'
   end
 end
