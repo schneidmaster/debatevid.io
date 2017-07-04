b7dfb49389cf97b80c93d87845f5b076d1513ec9
Zach Schneider <zach@schneid.io>
2017-07-04 15:28:18 -0400

    Add images to nav links

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -4,4 +4,16 @@ $(document).ready(() => {
   $('.nav-login-btn').on('click', () => {
     $('.nav-dropdown').toggleClass('hidden');
   });
+
+  $(document).on('click', (e) => {
+    if($(e.target).parents('.dropdown').length !== 0) {
+      return;
+    }
+
+    if($('.nav-dropdown').is('hidden')) {
+      return;
+    }
+
+    $('.nav-dropdown').addClass('hidden');
+  });
 });
