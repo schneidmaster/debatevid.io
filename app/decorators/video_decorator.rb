9785deb6ff42a464316b752ed7e880b4d87d9151
Zach Schneider <zach@schneid.io>
2017-07-30 18:32:39 -0400

    Fix cops

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -7,18 +7,18 @@ class VideoDecorator < Draper::Decorator
       src += "&playlist=#{key.drop(1).join(',')}" unless key.drop(1).empty?
 
       h.content_tag(:iframe, nil,
-                  id: 'vidframe',
-                  type: 'text/html',
-                  src: src,
-                  frameborder: '0')
+                    id: 'vidframe',
+                    type: 'text/html',
+                    src: src,
+                    frameborder: '0')
     else
       h.content_tag(:iframe, nil,
-                  id: 'vidframe',
-                  src: "https://player.vimeo.com/video/#{key.first}",
-                  frameborder: '0',
-                  webkitallowfullscreen: 'true',
-                  mozallowfullscreen: 'true',
-                  allowfullscreen: 'true')
+                    id: 'vidframe',
+                    src: "https://player.vimeo.com/video/#{key.first}",
+                    frameborder: '0',
+                    webkitallowfullscreen: 'true',
+                    mozallowfullscreen: 'true',
+                    allowfullscreen: 'true')
     end
   end
 end
