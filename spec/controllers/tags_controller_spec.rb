7719cbd115386c01a6c8f0c21a1b445a6167593c
Zach Schneider <zach@schneid.io>
2017-08-27 16:19:02 -0400

    Fix spec

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,7 +1,7 @@
 describe TagsController do
-  let(:tag) { create(:tag) }
-  let(:video) { create(:video, tags: [tag]) }
-  let(:other_video) { create(:video) }
+  let!(:tag) { create(:tag) }
+  let!(:video) { create(:video, tags: [tag]) }
+  let!(:other_video) { create(:video) }
 
   describe '#show' do
     subject(:request) { get :show, params: { id: tag.title } }
